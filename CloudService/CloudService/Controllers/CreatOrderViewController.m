7e1e45e780d770c9cc48a1d270c44ae2c019e91b
安永超 <ayc_mail@163.com>
2016-03-11 20:45:20 +0800

    1

--- CloudService/CloudService/Controllers/CreatOrderViewController.m
+++ CloudService/CloudService/Controllers/CreatOrderViewController.m
@@ -69,15 +69,16 @@
     
     [self resignKeyBoardInView:self.view];
     
-    __block ZQCityPickerView *cityPickerView = [[ZQCityPickerView alloc] initWithProvincesArray:nil cityArray:nil];
+    __block ZQCityPickerView *cityPickerView = [[ZQCityPickerView alloc] initWithProvincesArray:nil cityArray:nil componentsCount:2];
     
-    [cityPickerView showPickViewAnimated:^(NSString *province, NSString *city,NSString *cityCode) {
+    [cityPickerView showPickViewAnimated:^(NSString *province, NSString *city,NSString *cityCode,NSString *provinceCode) {
         self.tfCarCity.text = [NSString stringWithFormat:@"%@ %@",province,city];
         NSLog(@"%@",cityCode);
         cityPickerView = nil;
     }];
     
-    
+
+
 }
 /** 消失键盘*/
 - (void)resignKeyBoardInView:(UIView *)view
