a315b6a8b8e78657afd58a5014dfa45274560e15
Ctrlzq <15027164687@163.com>
2016-03-21 16:28:05 +0800

    push

--- CloudService/CloudService/Controllers/SetUserInfoViewController.h
+++ CloudService/CloudService/Controllers/SetUserInfoViewController.h
@@ -10,7 +10,14 @@
 
 @interface SetUserInfoViewController : BaseViewController
 
+/**
+ *  个人中心用户认证和注册成功进入,显示提交
+ *  其他地方进入,显示保存
+ */
 @property(nonatomic,strong)NSString *rightBtnTitle;
+/**
+ *  是否处于不可编辑状态
+ */
 @property(nonatomic,assign)BOOL notEnable;
 
 @end
